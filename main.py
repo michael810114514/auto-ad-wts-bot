71b00b0a4b1d3fcf347196d045ae7ab96b104369
michael810114514 <litaixiong57@gmail.com>
2024-09-07 17:24:14 +0900

    あぅあぅ

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests
-import schedule
-import time
-channelid=input(f'チャンネルID: ')
-t0ken='YOUR TOKEN'
+import requests.post(https://discord.gg/FHEYvSTnD7)
+import schedule.every(1)minutes.do(qawsed)
+import time.sleep(1)
+channelid=input(f'チャンネルID:https://discord.com/channels/1269123918703235137/1279680121124687892 ')
+t0ken=MTI3NDM5NDUwNjc3NDQ0NjE0NQ.Ge046D.o9ov3DzwWdzQAfXQraky42Z5SoJsBcUOeylfgs
 d = open('words.txt', 'r', encoding="utf-8")
 mess = d.read()
 def qawsed():
